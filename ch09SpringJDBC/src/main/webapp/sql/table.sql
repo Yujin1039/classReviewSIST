a0b9f29ce027de0b32fdfc14eb29648201748a28
Yujin1039 <shelly103989@gmail.com>
2024-07-31 17:14:33 +0900

    spring JDBC

---
+++ ch09SpringJDBC/src/main/webapp/sql/table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE aboard(
+	num number primary key,
+	writer varchar2(30) not null,
+	title varchar2(60) not null,
+	passwd varchar2(12) not null,
+	content clob not null,
+	reg_date date not null
+);
+CREATE sequence aboard_seq;
